670850912655f62350deb0848f93a49cf4bbd006
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2016-09-26 20:19:57 +0900

    midiMessageの取得には成功したがバインディングされない。多分Midiイベントがemitされていない

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `
+    selector: 'my-app',
+    template: `
   <h1>{{title}}</h1>
   <p>midi command: 0x{{midiCmd}}</p>
   <p>note number: {{noteNumber}}</p>
@@ -10,47 +10,43 @@ import {Component} from '@angular/core';
   `,
 })
 export class AppComponent {
-  title: string = 'Hello World!';
+    title: string = 'Hello World!';
 
-  inputs:any[] = [];
-  outputs:any[] = [];
-  navigator: Navigator = window.navigator;
+    inputs: WebMidi.MIDIInput[];
+    outputs: WebMidi.MIDIOutput[];
+    navigator: Navigator = window.navigator;
 
-  midiCmd: string;
-  noteNumber: number;
-  velocity: number;
+    midiCmd: string = '';
+    noteNumber: number = 0;
+    velocity: number = 0;
 
-  private MIDI_MESSAGE_INDEX: any = {
-    MIDI_COMMAND: 0,
-    NOTE_NUMBER: 1,
-    VELOCITY: 2
-  };
 
-  // TODO MID Accessに関する処理をサービス化して外に出す?
-  constructor() {
+    // TODO MID Accessに関する処理をサービス化して外に出す?
+    constructor() {
+        this.inputs = [];
+        this.outputs = [];
+        this.navigator.requestMIDIAccess().then((midi: WebMidi.MIDIAccess) => {
 
-    this.navigator.requestMIDIAccess().then(this.onMIDISuccess);
-  }
+            console.log(this);
 
-  onMIDISuccess(midi: WebMidi.MIDIAccess) {
-    let it = midi.inputs.values();
-    for (let i = it.next(); !i.done; i = it.next()) {
-      this.inputs.push(i.value);
+            let it = midi.inputs.values();
+            for (let i = it.next(); !i.done; i = it.next()) {
+                this.inputs.push(i.value);
+            }
+            let ot = midi.outputs.values();
+            for (let o = ot.next(); !o.done; o = ot.next()) {
+                this.outputs.push(o.value);
+            }
+            for (let cnt = 0; cnt < this.inputs.length; cnt++) {
+                this.inputs[cnt].onmidimessage = (e: WebMidi.MIDIMessageEvent) => {
+                    console.log(this);
+                    let midiMessages = e.data;
+                    this.midiCmd = midiMessages[0].toString(16);
+                    this.noteNumber = midiMessages[1];
+                    this.velocity = midiMessages[2];
+                };
+            }
+        });
     }
-    let ot = midi.outputs.values();
-    for (let o = ot.next(); !o.done; o = ot.next()) {
-      this.outputs.push(o.value);
-    }
-    for (let cnt = 0; cnt < this.inputs.length; cnt++) {
-      this.inputs[cnt].onmidimessage = this.onMIDIEvent;
-    }
-  }
 
-  onMIDIEvent(e: WebMidi.MIDIMessageEvent) {
-
-    let midiMessages = e.data;
-    this.midiCmd = midiMessages[this.MIDI_MESSAGE_INDEX.MIDI_COMMAND].toString(16);
-    this.noteNumber = midiMessages[this.MIDI_MESSAGE_INDEX.NOTE_NUMBER];
-    this.velocity = midiMessages[this.MIDI_MESSAGE_INDEX.VELOCITY];
-  }
 }
