c9c1e3597ab956a46016e49605f826a9b34bfac7
KadoTomohiro <corner0427@gmail.com>
2016-09-28 14:03:46 +0900

    midiServiceの整理

--- src/services/midiService.ts
+++ src/services/midiService.ts
@@ -4,29 +4,20 @@ import {NavigatorRef} from './navigatorRef';
 @Injectable()
 export class MidiService {
     @Output() midiIn: EventEmitter<any>;
-    public inputs:  WebMidi.MIDIInput[];
-    public outputs:  WebMidi.MIDIOutput[];
+    public inputs: Map<string, WebMidi.MIDIInput>;
 
     constructor(private navRef: NavigatorRef) {
 
-        this.inputs = [];
-        this.outputs = [];
         this.midiIn = new EventEmitter();
         navRef.navigator.requestMIDIAccess().then((midi: WebMidi.MIDIAccess) => {
-            let it = midi.inputs.values();
-            // midiInputの取得
-            for (let i = it.next(); !i.done; i = it.next()) {
-                let input = i.value;
-                // イベント処理の登録
-                input.onmidimessage = (e: WebMidi.MIDIMessageEvent) => this.midiIn.emit(e);
-                this.inputs.push(input);
-            }
-            // midiOutputの登録
-            let ot = midi.outputs.values();
-            for (let o = ot.next(); !o.done; o = ot.next()) {
-                this.outputs.push(o.value);
-            }
 
+            this.inputs = midi.inputs;
+
+            this.inputs.forEach(( input: WebMidi.MIDIInput) => {
+                input.onmidimessage = (e: WebMidi.MIDIMessageEvent) => this.midiIn.emit(e);
+            });
+
+            // midiOutputはmidi.outputsで取得できる
         });
     }
 
