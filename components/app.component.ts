0bb1a6a6db06f0ce73347321b39c70e5bdee2927
KadoTomohiro <kado.tomohiro@jp.fujitsu.com>
2016-11-25 16:15:48 +0900

    キーボードコンポーネント追加

---
+++ components/app.component.ts
@@ -0,0 +1,65 @@
+import {Component} from '@angular/core';
+// import {WaveComponent} from './wave/wave.component';
+import {MidiService, MidiMessage} from '../services/midiService';
+import {AudioService} from '../services/audioService';
+
+@Component({
+    selector: 'my-app',
+    template: `
+  <h1>{{title}}</h1>
+  <p>midi status: 0x{{midiStatus}}</p>
+  <p>note number: {{noteNumber}}</p>
+  <p>velocity: {{velocity}}</p>
+  <p>frequency: {{frequency}}Hz</p>
+  <keyboard (midiMessage)="onMidiMessage($event)"></keyboard>
+  <!--<select name="" id="" [(ngModel)]="oscType" (change)="setOscType($event.target.value)">-->
+    <!--<option *ngFor="let type of oscillatorType" [value]="type">{{type}}</option>-->
+  <!--</select>-->
+    
+    <synthesizer [message]="message"></synthesizer>
+  `,
+})
+export class AppComponent {
+    title: string = 'Midi Sample';
+
+    midiStatus: string;
+    noteNumber: number;
+    velocity: number;
+    frequency: number;
+    // oscType: string;
+
+    message: MidiMessage;
+
+
+    // oscillatorType: Array<string>;
+
+    constructor(private midiService: MidiService) {
+
+
+        this.midiService.midiIn.subscribe((e: WebMidi.MIDIMessageEvent) => this.onMidiMessage(e.data));
+
+        // this.oscillatorType = audioService.OscillatorType;
+        // this.oscType = this.audioService.oscType;
+
+    }
+
+    onMidiMessage(data: Uint8Array): void {
+        let message = new MidiMessage(data);
+        this.midiStatus = message.status.toString(16);
+        this.noteNumber = message.noteNo;
+        this.velocity = message.velocity;
+        this.frequency = Math.round(message.frequency * 100) / 100;
+
+        this.message = message;
+
+
+    }
+
+
+
+    // setOscType(type):void {
+    //     this.audioService.oscType = type;
+    // }
+
+}
+
